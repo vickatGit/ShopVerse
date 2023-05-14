1dd01938b0e19ba2b71ecb11a948c1de07f35870
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-05-14 18:19:13 +0530

    consumer auth is implemented

---
+++ controllers/consumerController.js
@@ -0,0 +1,86 @@
+const jwt = require("jsonwebtoken");
+
+const consumer = require("../models/consumerModel");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const registerConsumer = async (req, res, next) => {
+  try {
+    const { email, name, password } = req.body;
+
+    if (email && name && password) {
+      const hashedPassword = await bcrypt.hash(password, 6);
+      await consumer.create({
+        email: email,
+        consumerName: name,
+        password: hashedPassword,
+      });
+      res.status(201);
+      res.json({
+        email: email,
+        sellerName: name,
+        message: "Registration Successful"
+      });
+    } else {
+      res.status(400);
+      //   throw new Error("Invalid data");
+      res.json({
+        message: "Invalid Data"
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+const loginConsumer = async (req,res,next) => {
+  try {
+    const { email, name, password } = req.body;
+    if (email && name && password) {
+      const user = await consumer.findOne({ email: email });
+      if (user) {
+        const isVerified = await bcrypt.compare(password, user.password);
+        if (isVerified) {
+          const token = await jwt.sign(
+            {
+              email,
+              name
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "10d" }
+          );
+          res.status(201);
+          res.json({
+            message: "Successfully Logged in",
+            token: token,
+          });
+        } else {
+          res.status(400);
+          //   throw new Error("Wrong Creedentials");
+          res.json({
+            message: "Wrong Credentials",
+          });
+        }
+      } else {
+        res.status(400);
+        // throw new Error("Registration Required");
+        res.json({
+          message: "Registration Required",
+        });
+      }
+    } else {
+      res.status(400);
+      //   throw new Error("Invalid data");
+      res.json({
+        message: "Invalid Data",
+      });
+    }
+  } catch (error) {
+    // next(error);
+    console.log(error)
+  }
+};
+
+module.exports = {
+  registerConsumer,
+  loginConsumer,
+};
--- routes/consumerRoutes.js
+++ routes/consumerRoutes.js
@@ -1,6 +1,10 @@
-const router=require('express').Router()
+const router = require("express").Router();
+const {
+  registerConsumer,
+  loginConsumer,
+} = require("../controllers/consumerController");
 
-router.route("/register").post()
-router.route("/login").post()
+router.route("/register").post(registerConsumer);
+router.route("/login").post(loginConsumer);
 
-module.exports = router
+module.exports = router;
